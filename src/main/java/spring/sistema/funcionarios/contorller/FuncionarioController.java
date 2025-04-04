6a0828759991cfee0378028f6953007069657deb
gustavoKopp <gustavo.kopp@sisplansistemas.com.br>
2025-04-04 07:09:35 -0300

    Feat: Add Controller

---
+++ src/main/java/spring/sistema/funcionarios/contorller/FuncionarioController.java
@@ -0,0 +1,49 @@
+package spring.sistema.funcionarios.contorller;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import spring.sistema.funcionarios.model.Funcionario;
+import spring.sistema.funcionarios.service.FuncionarioService;
+
+@RestController
+@RequestMapping("/funcionarios")
+public class FuncionarioController {
+	@Autowired
+	private FuncionarioService service;
+	
+	@GetMapping
+	public List<Funcionario> listarTodos(){
+		return service.listarTodos();
+	}
+	
+	@PostMapping
+	public Funcionario salvar (@RequestBody Funcionario funcionario) {
+		return service.salvar(funcionario);
+	}
+	
+	@PutMapping("/{id}")
+	public Funcionario atualizar(@RequestBody Funcionario funcionario, @PathVariable Long id ) {
+		return service.alterar(funcionario, id);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Map<String, String>> deletar(@PathVariable Long id) {
+	    Map<String, String> response = new HashMap<>();
+	    try {
+	        service.deletar(id);
+	        response.put("message", "Funcionário deletado com sucesso!");
+	        return ResponseEntity.status(HttpStatus.OK).body(response);
+	    } catch (EmptyResultDataAccessException e) {
+	        response.put("message", "Funcionário não encontrado!");
+	        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+	    }
+	}
+}
