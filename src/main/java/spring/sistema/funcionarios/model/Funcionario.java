6b45b0182ccea037cc1689b96407f6f155a5503e
gustavoKopp <gustavo.kopp@sisplansistemas.com.br>
2025-04-04 07:09:57 -0300

    Feat: Add Model

---
+++ src/main/java/spring/sistema/funcionarios/model/Funcionario.java
@@ -0,0 +1,55 @@
+package spring.sistema.funcionarios.model;
+
+import jakarta.persistence.*; 
+
+@Entity
+public class Funcionario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+	private Double salario;
+	private String funcao;
+	private String setor;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void SetId(Long id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public Double getSalario() {
+		return salario;
+	}
+	
+	public void setSalario(Double salario) {
+		this.salario = salario;
+	}
+	
+	public String getFuncao() {
+		return funcao;
+	}
+	
+	public void setFuncao(String funcao) {
+		this.funcao = funcao;
+	}
+	
+	public String getSetor() {
+		return setor;
+	}
+	
+	public void setSetor(String setor) {
+		this.setor = setor;
+	}
+}
