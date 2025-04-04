e12ec1e04af91529b52218a243d9727e7d1e6cab
gustavoKopp <gustavo.kopp@sisplansistemas.com.br>
2025-04-04 08:46:46 -0300

    Feat: Ajustado para poder usar Query

--- src/main/java/spring/sistema/funcionarios/model/Funcionario.java
+++ src/main/java/spring/sistema/funcionarios/model/Funcionario.java
@@ -3,6 +3,7 @@ package spring.sistema.funcionarios.model;
 import jakarta.persistence.*; 
 
 @Entity
+@Table(name = "funcionario")
 public class Funcionario {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
